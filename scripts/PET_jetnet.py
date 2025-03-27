Refactor loss calculation in PET_jetnet.py to enhance per-event loss computation and ensure correct handling of weights

scripts/PET_jetnet.py
@@ -234,7 +234,6 @@ class PET_jetnet(keras.Model):
             target = x_clean
 
             per_event_loss = loss_weight * tf.square(pred - target)
-
             if weight is not None:
                 weight = tf.expand_dims(weight, axis=-1)
                 loss = tf.reduce_sum(weight * per_event_loss) / tf.reduce_sum(weight)
@@ -290,9 +289,12 @@ class PET_jetnet(keras.Model):
         )
         target = x_clean
 
-        loss = tf.reduce_mean(loss_weight * tf.square(pred - target))
+        per_event_loss = loss_weight * tf.square(pred - target)
         if weight is not None:
-            loss = tf.reduce_sum(weight * loss) / tf.reduce_sum(weight)
+            weight = tf.expand_dims(weight, axis=-1)
+            loss = tf.reduce_sum(weight * per_event_loss) / tf.reduce_sum(weight)
+        else:
+            loss = tf.reduce_mean(per_event_loss)
 
         self.loss_tracker.update_state(loss)
         self.sigma_tracker.update_state(sigma)
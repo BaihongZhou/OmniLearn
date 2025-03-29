300fc86b0fdd9747429839fe51cfc0e7a4ebb50b
avencastmini <yulei@cern.ch>
2025-03-29 09:36:47 +0100

    Refactor neutrino processing in evaluate.py and loss calculation in PET_jetnet.py to adjust dimensions and improve loss computation

--- scripts/PET_jetnet.py
+++ scripts/PET_jetnet.py
@@ -295,7 +295,8 @@ class PET_jetnet(keras.Model):
         per_event_loss = loss_weight * tf.square(pred - target)
         if weight is not None:
             weight = tf.expand_dims(weight, axis=-1)
-            loss = tf.reduce_sum(weight * per_event_loss) # / tf.reduce_sum(weight)
+            # loss = tf.reduce_sum(weight * per_event_loss) # / tf.reduce_sum(weight)
+            loss = tf.reduce_mean(weight * per_event_loss)
         else:
             loss = tf.reduce_mean(per_event_loss)
 
--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -79,7 +79,7 @@ def sample_data(eval_dataloader, model, sample_name, raw_particle_list, split: b
     )
 
     final_neutrinos = [
-        eval_dataloader.revert_preprocess_neutrino(raw_nu_candidates[:, i]).reshape(-1, 1, 12)
+        eval_dataloader.revert_preprocess_neutrino(raw_nu_candidates[:, i]).reshape(-1, 1, 8)
         for i in range(raw_nu_candidates.shape[1])
     ]
     final_neutrinos = np.concatenate(final_neutrinos, axis=1)
@@ -102,7 +102,7 @@ def sample_data(eval_dataloader, model, sample_name, raw_particle_list, split: b
             data_dict = {
                 'recon_nu1': final_neutrinos[:, :, :4],
                 'recon_nu2': final_neutrinos[:, :, 4:8],
-                'diff': final_neutrinos[:, :, 8:],
+                # 'diff': final_neutrinos[:, :, 8:],
             }
             data_dict.update({
                 f"extra_{i}": extra_info[:, i]
@@ -119,7 +119,7 @@ def sample_data(eval_dataloader, model, sample_name, raw_particle_list, split: b
                 data_dict = {
                     'nu1': final_neutrinos[mask][:, :, :4],
                     'nu2': final_neutrinos[mask][:, :, 4:8],
-                    'diff': final_neutrinos[mask][:, :, 8:],
+                    # 'diff': final_neutrinos[mask][:, :, 8:],
                 }
                 data_dict.update({
                     f"extra_{i}": extra_info[mask, i]
